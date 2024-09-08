537dc5668335d6361c967d7ba8cc973de6b2178e
Abhaktaram <abhakta2@jhu.edu>
2024-09-08 13:49:55 -0400

    Initial commit

---
+++ Project 0.R
@@ -0,0 +1,8 @@
+### Biostats 776: Statistical Computing
+  # Assignment: Project O - Intro Website
+  # Author: Ananya Bhaktaram
+  # Date: September 8th, 2024
+
+# Load Relevant Packages
+
+library()
